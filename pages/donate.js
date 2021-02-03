53b1b31f672c8d4cb67e600e6820225810aef16d
philogynic <bimogame161@gmail.com>
2021-02-04 01:09:04 +0700

    create donation

--- pages/donate.js
+++ pages/donate.js
@@ -1,18 +1,33 @@
 import {useState} from 'react'
 import Link from 'next/link'
+import Router from 'next/router'
 import { useAuth } from '../lib/auth'
+import { useForm } from 'react-hook-form'
+import { createDonation } from '../lib/db'
 
 
 export default function Donate() {
   const auth = useAuth()
+
+  const {handleSubmit, register, errors} = useForm()
   
-  const onClick = () => {
-    console.log(auth.user)
-  }
+  const onSubmit = handleSubmit(async (formData) => {
+    const newDonation = {
+      donatorId: auth.user.uid,
+      donatorEmail: auth.user.email,
+      text: formData.text,
+      donationAmount: formData.donationAmount,
+      createdAt: new Date().toISOString()
+    }
+
+    await createDonation(newDonation)
+    Router.push('/dashboard')
+
+  })
 
   return (
       <div className='font-mono bg-gray-300 h-screen flex flex-col'>
-        <div className='w-64 bg-gray-100 mx-auto my-auto rounded-xl px-3 py-4 flex flex-col'>
+        <form onSubmit={onSubmit} className='w-64 bg-gray-100 mx-auto my-auto rounded-xl px-3 py-4 flex flex-col'>
           <div className='flex justify-end'>
               <Link href='/dashboard'>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
@@ -22,15 +37,36 @@ export default function Donate() {
               </Link> 
           </div>
           <div className='font-bold text-lg text-gray-700'>pesen2nya, <span className='text-yellow-400'>bro</span>:</div>
-          <textarea className='w-full rounded-md p-1' rows='4'
-            placeholder='ngntod lo hahaha...'></textarea>
+          <textarea 
+            className='w-full rounded-md p-1' 
+            rows='4'
+            placeholder='ngntod lo hahaha...'
+            name='text'
+            ref={register()}
+            ></textarea>
           {auth.user ? (<div>dari <span className='font-bold break-words'>{auth.user.email}</span></div>):(<div></div>)}
-          <div className='flex font-bold justify-between mt-6'>
+          {/* <div className='flex font-bold justify-between mt-6'>
             <div className='bg-indigo-100 px-5 py-1 rounded-xl text-indigo-500'>5k</div>
             <div className='bg-indigo-100 px-5 py-1 rounded-xl text-indigo-500'>10k</div>
             <div className='bg-indigo-100 px-5 py-1 rounded-xl text-indigo-500'>50k</div>
-          </div>
-          <div onClick={onClick} className='bg-indigo-100 font-bold text-2xl px-5 py-1 rounded-xl text-indigo-500 text-center mt-4'>kasih duit</div>
+          </div> */}
+
+          <label className="block mt-4">
+            <span className="text-gray-700">jumlah donasi</span>
+            <select className="form-select mt-1 block w-full" name='donationAmount' ref={register}>
+              <option>$1,000</option>
+              <option>$5,000</option>
+              <option>$10,000</option>
+              <option>$25,000</option>
+            </select>
+          </label>
+
+          <button 
+            type='submit'
+            className='bg-indigo-100 font-bold text-2xl px-5 py-1 rounded-xl text-indigo-500 text-center mt-4'
+            >
+              kasih duit
+          </button>
           {auth.user ? (
             <div className='font-bold text-md text-gray-700 mt-4 mb-4 text-center'>
               <button onClick={() => {auth.signout()}} className='text-yellow-400 underline'>
@@ -41,7 +77,7 @@ export default function Donate() {
             <div className='font-bold text-md text-gray-700 mt-4 mb-4 text-center'> <Link href='/login'><a className='text-yellow-400 underline'>login</a></Link> biar ga anonymous</div>
           )}
           
-        </div>
+        </form>
       </div>
     )
   }
